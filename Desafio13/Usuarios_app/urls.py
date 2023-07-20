235362d06659cbed733c1943120bb378cee1df30
JoseAFi <jofiguer@gmail.com>
2023-07-20 15:23:09 -0300

    probando arreglar todo

--- Desafio13/Usuarios_app/urls.py
+++ Desafio13/Usuarios_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path 
 from usuarios_app import views
 from django.contrib.auth.views import LoginView, LogoutView
-app_name = "usuarios_app"
+app_name = "usuario"
 
 urlpatterns = [
-	path('login/', LoginView.as_view(template_name = 'login.html', name = 'login')),
+	path('login/', LoginView.as_view(template_name = 'usuarios/login.html', name = 'login')),
     path('logout/', LogoutView.as_view(next_page= 'login/',name = 'logout'))   
 ] 
--- Desafio13/core/urls.py
+++ Desafio13/core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
 	path('index/', views.indexView, name = 'index'),
-#    path('usuario/', include('usuarios_app.urls'), name = 'usuario'),
+    path('usuario/', include('usuarios_app.urls'), name = 'usuario'),
 ] 
