06f6f19e4940d48c0c39b1ca442619ad45ff1379
JoseAFi <jofiguer@gmail.com>
2023-07-20 10:50:03 -0300

    correcciones base

--- Desafio13/Usuarios_app/models.py
+++ Desafio13/Usuarios_app/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
 class Usuario(AbstractBaseUser):
-    es_admin = models.BooleanField(default=False),
     es_usuario = models.BooleanField(default=True),
     es_colaborador = models.BooleanField(default=False),
+    es_admin = models.BooleanField(default=False),
