27c189588272afdbd5e1da22729852223517f4be
Fernando Javier Svoboda <svofer333@gmail.com>
2023-07-20 10:21:25 -0300

    add_models
    Creacion tabla usuarios

--- Desafio13/Usuarios_app/models.py
+++ Desafio13/Usuarios_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+class Usuario(AbstractBaseUser):
+    es_admin = models.BooleanField(default=False),
+    es_usuario = models.BooleanField(default=True),
+    es_colaborador = models.BooleanField(default=False),
