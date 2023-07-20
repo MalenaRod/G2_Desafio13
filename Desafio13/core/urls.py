7bdff96f2fcc31b25c0a9c02ed0fe9c1cc2a265e
Fernando Javier Svoboda <svofer333@gmail.com>
2023-07-20 14:58:53 -0300

    inicio

--- Desafio13/core/urls.py
+++ Desafio13/core/urls.py
@@ -2,6 +2,6 @@ from core import views
 from django.urls import path, include
 
 urlpatterns = [
-	path('index/', views.indexView, name = 'index'),
+	path('', views.indexView, name = 'index'),
 #    path('usuario/', include('usuarios_app.urls'), name = 'usuario'),
 ] 
