c49f21fac19e4c859ab74c3dc5dac7bb6a828e5f
JoseAFi <jofiguer@gmail.com>
2023-07-20 10:21:48 -0300

    corrige html

--- Desafio13/Usuarios_app/views.py
+++ Desafio13/Usuarios_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Usuario
-from .forms import RegistrarseForm
+from django.forms import RegistrarseForm
 from django.urls import reverse
 
 # Create your views here.
@@ -9,7 +9,7 @@ from django.urls import reverse
 # Vista basada en una clase para crear a un usuario
 class RegistroView(CreateView):
     model = Usuario
-    template_name = 'usuarios/registro.html'
+    template_name = 'usuarios/login.html'
     form_class = RegistrarseForm #UserCreationForm (despues lo cambia)
 
     def get_success_url(self):  # Esto es para que vuelva a pagina Inicio o Index.
