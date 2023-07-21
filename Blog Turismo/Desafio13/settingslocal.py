2c3999bf0abd94603f90bea6c1375856029e32cb
Fernando Javier Svoboda <svofer333@gmail.com>
2023-07-21 17:37:47 -0300

    Fusion brand

---
+++ Blog Turismo/Desafio13/settingslocal.py
@@ -0,0 +1,12 @@
+from .settingsbase import *
+
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'blog', 
+        'USER': 'root', 
+        'PASSWORD': '364328548a', 
+        'HOST': 'localhost', 
+        'PORT': '3306', 
+    } 
+} 
