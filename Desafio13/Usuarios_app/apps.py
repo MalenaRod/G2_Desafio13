0fa267e7b1a60c3ea32cd29a2d87f2c9c249988c
JoseAFi <jofiguer@gmail.com>
2023-07-20 22:07:35 -0300

    subo cambios usuarios_app

--- Desafio13/Usuarios_app/apps.py
+++ Desafio13/Usuarios_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class UsuariosAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'usuarios_app'
+    name = 'Usuarios_app'
--- Desafio13/Usuarios_app/urls.py
+++ Desafio13/Usuarios_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 from django.contrib.auth.views import LoginView, LogoutView
-app_name = "usuarios_app"
+app_name = "usuarios"
 
 urlpatterns = [
 	path('login/', LoginView.as_view(template_name = 'usuarios/login.html'), name = 'login'),
