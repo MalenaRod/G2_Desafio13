82f32e43f316bcbef30675fe7265f53363f076ee
JoseAFi <jofiguer@gmail.com>
2023-07-24 14:28:09 -0300

    agrega imagen_usuario

--- Blog Turismo/Usuarios_app/forms.py
+++ Blog Turismo/Usuarios_app/forms.py
@@ -5,4 +5,4 @@ from .models import Usuario
 class RegistrarseForm(UserCreationForm):
     class Meta:
         model = Usuario
-        fields = ['first_name', 'last_name', 'username', 'password1', 'password2', 'email']
+        fields = ['first_name', 'last_name', 'username', 'password1', 'password2', 'email','imagen']
