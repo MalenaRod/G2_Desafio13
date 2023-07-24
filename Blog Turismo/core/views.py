026da5c9372be10c07234f6165799053a9f3aa3b
Antonella Ruelli <antonellaruelli@gmail.com>
2023-07-24 17:13:23 -0300

    Se agregó templates de contacto y el botón de contacto quedo funcionandon

--- Blog Turismo/core/views.py
+++ Blog Turismo/core/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def indexView(request):
-	return render(request, 'index.html', {} ) 
+	return render(request, 'index.html', {} )
+
+#vista contactenos
+
+def vistaContactenos(request):
+    return render(request, 'contacto.html', {})
+
