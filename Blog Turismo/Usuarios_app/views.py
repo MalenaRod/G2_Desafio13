9185316cf962e4de60acfb621fff6f2fd50ba4c3
JoseAFi <jofiguer@gmail.com>
2023-07-24 14:37:40 -0300

    sige usuario

--- Blog Turismo/Usuarios_app/views.py
+++ Blog Turismo/Usuarios_app/views.py
@@ -3,6 +3,7 @@ from django.views.generic.edit import CreateView
 from .models import Usuario
 from .forms import RegistrarseForm
 from django.urls import reverse
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -15,4 +16,8 @@ class RegistroView(CreateView):
     def get_success_url(self):  # Esto es para que vuelva a pagina Inicio o Index.
         return reverse('index') 
     
-    
+    def form_valid(self, form):
+        respuesta = super().form_valid(form)
+        usuario = form.save()
+        login(self.request, usuario)
+        return respuesta
