68a36b8fd000039b47a475b561c2325e89cd2c6f
JoseAFi <jofiguer@gmail.com>
2023-07-27 12:20:18 -0300

    sin es_admin

--- Blog Turismo/Usuarios_app/models.py
+++ Blog Turismo/Usuarios_app/models.py
@@ -5,6 +5,5 @@ from django.contrib.auth.models import AbstractUser
 class Usuario(AbstractUser):
     es_usuario = models.BooleanField(default=True)
     es_colaborador = models.BooleanField(default=False)
-    es_admin = models.BooleanField(default=False)
     imagen = models.ImageField(upload_to='imagenes_pefil', blank=True, null=True)
 
