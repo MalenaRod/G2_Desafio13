2408831f1c475fbc6e5d8e310c3b528c643b14c9
Antonella Ruelli <antonellaruelli@gmail.com>
2023-07-30 22:15:30 -0300

    cambios es formulario de contacto

--- Blog Turismo/core/forms.py
+++ Blog Turismo/core/forms.py
@@ -5,12 +5,25 @@ class ContactoForm(forms.ModelForm):
     class Meta:
         model = MensajeContacto
         fields = ['nombre', 'correo', 'pais', 'edad', 'mensaje']
+        labels={
+            'nombre': '',
+            'correo': '',
+            'pais':'',
+            'edad': '',
+            'mensaje':''
+        }        
+        
+        widgets={
+           'nombre': forms.TextInput(attrs= {'class':'form-control', 'placeholder': 'Nombre'}),
+           'correo': forms.TextInput(attrs= {'placeholder': 'Correo electrónico', 'class':'form-control'}),
+           'edad': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Edad'}),
+           'mensaje': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Deje su consulta aquí...'}),                                
+        }
+           
 
     PAISES_CHOICES = [
-        ('Argentina', 'Argentina'),
-        ('Brasil', 'Brasil'),
-        ('Chile', 'Chile'),
-        # Agrega más países según tu preferencia...
+        ('Argentina', 'Argentina'), ('Bolivia', 'Bolivia'), ('Brasil', 'Brasil'), ('Chile', 'Chile'), ('Colombia', 'Colombia'), ('Ecuador', 'Ecuador'), ('Paraguay', 'Paraguay'), ('Perú', 'Perú'), ('Uruguay', 'Uruguay'), ('Venezuela', 'Venezuela'),  ('Centro America', 'Centro America'),  ('Europa', 'Europa'),  ('America del Norte', 'America del Norte'), ('Asia', 'Asia'), ('Otros', 'Otros'),
+
     ]
 
     pais = forms.ChoiceField(choices=PAISES_CHOICES)
